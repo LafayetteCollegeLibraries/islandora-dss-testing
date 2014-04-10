bd01c6cbfb46165549681123b740e3a3316089cd
jrgriffiniii <griffinj@lafayette.edu>
2014-04-10 16:09:46 -0400

    DSSSM-706 #time 9m Abstracting tests for all sorting fields for the EAIC

--- spec/solr_browse_sort_format_medium_complex.rb
+++ spec/solr_browse_sort_format_medium_complex.rb
@@ -23,45 +23,57 @@ describe "SolrBrowseSortFormatMediumComplex" do
   end
 
   describe "Refining browsed items sorted by fields" do
-  [
-   
-   {"Subject.OCM" => "eastasia.Subject.OCM.sort"},
-   {"Date.Artifact.Lower" => "eastasia.Date.Artifact.Lower"},
-   {"Date.Artifact.Upper" => "eastasia.Date.Artifact.Upper"},
-   {"Coverage.Location.Country" => "eastasia.Coverage.Location.Country.sort"},
-   # {"Creator.Maker" => "eastasia.Creator.Maker.sort"},
-   {"Format.Medium" => "eastasia.Format.Medium.sort"},
-  ].each do |field|
 
-      it "should be able to retain sorting by #{field.keys.shift} after refining by a facet" do
+    collection_fields = [
+                         { "East Asia Image Collection" => [
+                                                            {"Subject.OCM" => "eastasia.Subject.OCM.sort"},
+                                                            {"Date.Artifact.Lower" => "eastasia.Date.Artifact.Lower"},
+                                                            {"Date.Artifact.Upper" => "eastasia.Date.Artifact.Upper"},
+                                                            {"Coverage.Location.Country" => "eastasia.Coverage.Location.Country.sort"},
+                                                            # {"Creator.Maker" => "eastasia.Creator.Maker.sort"},
+                                                            {"Format.Medium" => "eastasia.Format.Medium.sort"},
+                                                           ]},
+                        ].each do |collection_fields|
 
-        @driver.get(@base_url)
-        @driver.find_element(:link, "Collections").click
-        @driver.find_element(:link, "East Asia Image Collection").click
-        @driver.find_element(:link, field.keys.shift).click
-        (@driver.find_element(:id, "field-sort-select").attribute("value")).should == field.values.shift
+      collection_name = collection_fields.keys.shift
 
-        !60.times{ break if (element_present?(:css, "p.lead") rescue false); sleep 1 }
-        @driver.find_element(:link, "Imperial Postcard Collection").click
-        !60.times{ break if (element_present?(:css, "p.lead") rescue false); sleep 1 }
-        (@driver.find_element(:id, "field-sort-select").attribute("value")).should == field.values.shift
-      end
+      describe "Refining browsed items sorted by fields within #{collection_name}" do
+
+        fields = collection_fields.values.shift
+
+        fields.each do |field|
+
+          it "should be able to retain sorting by #{field.keys.shift} after refining by a facet" do
+
+            @driver.get(@base_url)
+            @driver.find_element(:link, "Collections").click
+            @driver.find_element(:link, "East Asia Image Collection").click
+            @driver.find_element(:link, field.keys.shift).click
+            (@driver.find_element(:id, "field-sort-select").attribute("value")).should == field.values.shift
+            
+            !60.times{ break if (element_present?(:css, "p.lead") rescue false); sleep 1 }
+            @driver.find_element(:link, "Imperial Postcard Collection").click
+            !60.times{ break if (element_present?(:css, "p.lead") rescue false); sleep 1 }
+            (@driver.find_element(:id, "field-sort-select").attribute("value")).should == field.values.shift
+          end
       
-      it "should be able to retain sorting by #{field.keys.shift} after refining by a date range" do
+          it "should be able to retain sorting by #{field.keys.shift} after refining by a date range" do
 
-        @driver.get(@base_url)
-        @driver.find_element(:link, "Collections").click
-        @driver.find_element(:link, "East Asia Image Collection").click
-        @driver.find_element(:link, field.keys.shift).click
-        (@driver.find_element(:id, "field-sort-select").attribute("value")).should == field.values.shift
-        @driver.find_element(:xpath, "//body[@onclick='if(stWidget.buttonClicked==false){stWidget.stopClosing=false;stWidget.openDuration=0;stWidget.stClose(100);}']").click
+            @driver.get(@base_url)
+            @driver.find_element(:link, "Collections").click
+            @driver.find_element(:link, "East Asia Image Collection").click
+            @driver.find_element(:link, field.keys.shift).click
+            (@driver.find_element(:id, "field-sort-select").attribute("value")).should == field.values.shift
+            @driver.find_element(:xpath, "//body[@onclick='if(stWidget.buttonClicked==false){stWidget.stopClosing=false;stWidget.openDuration=0;stWidget.stClose(100);}']").click
+            
+            # ERROR: Caught exception [ERROR: Unsupported command [dragAndDrop | //section[@id='block-islandora-solr-facet-pages-islandora-solr-facet-pages']/div[7]/a | +70,0]]
+            # @driver.find_element(:xpath, "//section[@id='block-islandora-solr-facet-pages-islandora-solr-facet-pages']/div[7]/a").drag_and_drop_by(70, 0)
+            @driver.action.drag_and_drop_by(@driver.find_element(:xpath, "//section[@id='block-islandora-solr-facet-pages-islandora-solr-facet-pages']/div[7]/a"), 70, 0).perform
 
-        # ERROR: Caught exception [ERROR: Unsupported command [dragAndDrop | //section[@id='block-islandora-solr-facet-pages-islandora-solr-facet-pages']/div[7]/a | +70,0]]
-        # @driver.find_element(:xpath, "//section[@id='block-islandora-solr-facet-pages-islandora-solr-facet-pages']/div[7]/a").drag_and_drop_by(70, 0)
-        @driver.action.drag_and_drop_by(@driver.find_element(:xpath, "//section[@id='block-islandora-solr-facet-pages-islandora-solr-facet-pages']/div[7]/a"), 70, 0).perform
-
-        !60.times{ break if (element_present?(:css, "p.lead") rescue false); sleep 1 }
-        (@driver.find_element(:id, "field-sort-select").attribute("value")).should == field.values.shift
+            !60.times{ break if (element_present?(:css, "p.lead") rescue false); sleep 1 }
+            (@driver.find_element(:id, "field-sort-select").attribute("value")).should == field.values.shift
+          end
+        end
       end
     end
   end
