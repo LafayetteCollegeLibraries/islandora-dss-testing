2d739333b8fa9ce4d4469951a344f1ad5a92ee98
jrgriffiniii <jrgriffiniii@gmail.com>
2014-04-10 22:29:10 -0400

    DSSSM-704 #time 36m Implementing CSV parsing for test case generation

--- spec/solr_advanced_search_collections.rb
+++ spec/solr_advanced_search_collections.rb
@@ -22,20 +22,38 @@ describe "SolrAdvancedSearchCollections" do
     @verification_errors.should == []
   end
 
-  {
-    'East Asia Image Collection' => {'Full Text' => {:value => 'taiwan', :results => 2170},
-    },
-  }.each_pair do |collection_name, collection_fields|
+  # Retrieve the values from the CSV
+  require 'csv'
+  collection_fixtures = {}
+
+  CSV.foreach("search_terms_for_selenium.csv", encoding: "ISO8859-1") do |row|
+
+    if collection_fixtures.has_key? row[0] and collection_fixtures[row[0]].has_key? row[1]
+
+      # collection_fixtures[row[0]][row[1]][:value] = row[2]
+      # collection_fixtures[row[0]][row[1]][:results] = row[3]
+      collection_fixtures[row[0]][row[1]] << { :value => row[2], :results => row[3] }
+
+    else
+      
+      # collection_fixtures[row[0]] = { row[1] => { :value => row[2], :results => row[3] } }
+      collection_fixtures[row[0]] = { row[1] => [ { :value => row[2], :results => row[3] } ] }
+    end
+  end
+
+  collection_fixtures.each_pair do |collection_name, collection_fields|
 
     describe "Search by all fields within #{collection_name}" do
 
-      collection_fields.each_pair do |field_name, field|
+      collection_fields.each_pair do |field_name, fields|
 
-        field_value = field[:value]
-        field_results = field[:results]
+        fields.each do |field|
 
-        describe "Search by all #{field_name} within #{collection_name}" do
+          field_value = field[:value]
+          field_results = field[:results]
 
+          describe "Search by all #{field_name} within #{collection_name}" do
+            
           # Attempt the following transformations:
           # lower case
           # upper case
@@ -48,25 +66,30 @@ describe "SolrAdvancedSearchCollections" do
            field_value.capitalize,
            field_value.swapcase,
           ].each do |field_value_i|
-  
+
             it "finds #{field_results} for #{field_value_i}" do
-              @driver.get(@base_url)
-              @driver.find_element(:link, "Collections").click
-              @driver.find_element(:link, "East Asia Image Collection").click
-              @driver.find_element(:link, "Title.English").click
-              @driver.find_element(:css, "img[alt=\"search the site\"]").click
-              #     @driver.find_element(:css, "option[value=\"#{field_name}\"]").click
-              @driver.find_element(:xpath, "//option[text()='#{field_name}']").click
-              @driver.find_element(:id, "edit-terms-0-field-wrapper-search").send_keys field_value_i
-              @driver.find_element(:id, "edit-submit--3").click
-              (@driver.find_element(:css, "p.lead").text).should == "#{field_results} Items Found"
+
+                @driver.get(@base_url)
+                @driver.find_element(:link, "Collections").click
+                @driver.find_element(:link, "East Asia Image Collection").click
+                @driver.find_element(:link, "Title.English").click
+                @driver.find_element(:css, "img[alt=\"search the site\"]").click
+                #     @driver.find_element(:css, "option[value=\"#{field_name}\"]").click
+                @driver.find_element(:xpath, "//option[text()='#{field_name}']").click
+                
+                # Necessary for clearing example date text
+                @driver.find_element(:id, "edit-terms-0-field-wrapper-search").click
+                @driver.find_element(:id, "edit-terms-0-field-wrapper-search").send_keys field_value_i
+                @driver.find_element(:id, "edit-submit--3").click
+                (@driver.find_element(:css, "p.lead").text).should == "#{field_results} Items Found"
+              end
             end
           end
         end
       end
     end
   end
-    
+  
   def element_present?(how, what)
     @driver.find_element(how, what)
     true
