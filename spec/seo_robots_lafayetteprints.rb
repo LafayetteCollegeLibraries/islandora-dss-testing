ddbac62cd5006605cc71255ca585465c76d3daa2
jrgriffiniii <griffinj@lafayette.edu>
2014-12-09 16:14:27 -0500

    DSSSM-1068 1h 30m Extending the testing suite for digital.lafayette.edu to include essential Selenium tests for the Solr browse interface for the "Alumni Publications" Collection

--- spec/seo_robots_lafayetteprints.rb
+++ spec/seo_robots_lafayetteprints.rb
@@ -24,7 +24,7 @@ describe "robots.txt restrictions for web crawlers" do
   end
 
   it 'permits faceted "series" within the Marquis de Lafayette Prints Collection to be crawled' do
-
-    expect(Robotstxt.allowed?('http://digital.stage.lafayette.edu/islandora/search/*:*?f[0]=cdm.Relation.IsPartOf:%22Marquis%20de%20Lafayette%20Prints%20Collection%22&f[1]=mdl_prints.description.series:%22Portraits:%20Before%201789%22', 'stage')).to be_true
+    
+    expect(Robotstxt.allowed?('http://digital.stage.lafayette.edu/islandora/search/q?f[0]=cdm.Relation.IsPartOf:Marquis&f[1]=mdl_prints.description.series:Portraits', 'stage')).to be_true
   end
 end
